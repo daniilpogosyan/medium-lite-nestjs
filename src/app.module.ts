6b4e203c3244a6a82c34e3142d91744c367088d2
Daniil Pogosyan <daniilpogosyan@gmail.com>
2023-01-07 13:00:22 +0500

    Clean boilerplate,  set structure of routes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,28 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { RouterModule } from '@nestjs/core';
+import { UsersModule } from './users/users.module';
+import { PostsModule } from './posts/posts.module';
+import { AccountModule } from './account/account.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    AccountModule,
+    UsersModule,
+    PostsModule,
+    RouterModule.register([
+      {
+        path: 'account',
+        module: AccountModule,
+      },
+      {
+        path: 'users',
+        module: UsersModule,
+      },
+      {
+        path: 'posts',
+        module: PostsModule,
+      }
+    ])
+  ]
 })
 export class AppModule {}
